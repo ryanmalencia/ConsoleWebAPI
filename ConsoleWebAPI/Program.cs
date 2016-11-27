09d323fbc30b165b594a910caaaa1993d0775044
ryanmalencia <ryan.malencia@pitt.edu>
2016-11-27 16:15:06 -0500

    Open Listening Port Automatically

--- ConsoleWebAPI/Program.cs
+++ ConsoleWebAPI/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin.Hosting;
 using System.Net;
 using System.Net.Sockets;
+using NetFwTypeLib;
 
 namespace ConsoleWebAPI
 {
@@ -11,7 +12,7 @@ namespace ConsoleWebAPI
         {
             //string baseAddress = "http://" + System.Environment.MachineName + ":9999/";
             string baseAddress = "http://" + GetLocalIPAddress() + ":9999/";
-
+            AllowPortAccess();
             using (WebApp.Start<Startup>(url: baseAddress))
             {
                 Console.WriteLine("WebAPI hosted at " + baseAddress);
@@ -35,6 +36,46 @@ namespace ConsoleWebAPI
                 }
             }
             throw new Exception("Local IP Address Not Found!");
+        }
+
+        public static void AllowPortAccess()
+        {
+            INetFwMgr icfMgr = null;
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            try
+            {
+                INetFwProfile profile;
+                INetFwOpenPort portClass;
+                Type TportClass = Type.GetTypeFromProgID("HNetCfg.FWOpenPort");
+                portClass = (INetFwOpenPort)Activator.CreateInstance(TportClass);
+
+                // Get the current profile
+                profile = icfMgr.LocalPolicy.CurrentProfile;
+
+                // Set the port properties
+                portClass.Scope = NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+                portClass.Enabled = true;
+                portClass.Protocol = NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+                portClass.Name = "ConsoleWebAPI";
+                portClass.Port = 9999;
+
+                // Add the port to the ICF Permissions List
+                profile.GloballyOpenPorts.Add(portClass);
+                return;
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
